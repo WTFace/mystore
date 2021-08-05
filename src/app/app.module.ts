40cc7d33669a68e9a2e8b137a53a5e6e3daf0103
wtface <nkhpark@hotmail.com>
2021-08-05 09:23:52 +0800

    feat: routing, nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductsComponent
+    ProductsComponent,
+    NavComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
